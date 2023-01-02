3180f2954942e7b6ab08171e06ca94368ab6a078
villenaj <93912972+villenaj@users.noreply.github.com>
2023-01-02 09:23:05 +0800

    Update getValue.php

--- public/getValue.php
+++ public/getValue.php
@@ -1,7 +1,7 @@
 <?php
 $q = intval($_GET['q']);
 
-$con = mysqli_connect('localhost','root','','qrattendance');
+$con = mysqli_connect('us-cdbr-east-06.cleardb.net','b3f163b9ed5cc0','e937938d','heroku_8f911c5e407f80b');
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
